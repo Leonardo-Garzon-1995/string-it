5b12a0505874f357cfdeb109c08dc8c8bba59aab
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-10-30 06:38:37 +0100

    simplified function

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -6,9 +6,7 @@ import reverseStr from "./reverseStr.js";
  * @returns {boolean} True if the string is a palindrome, false otherwise.
  */
 function isPalindrome(str) {
-    if (typeof str !== "string") {
-        throw new TypeError("isPalindrome() expects a string as its argument");
-    }
+    if (typeof str !== "string") return "";
     
     return str === reverseStr(str);
 }
